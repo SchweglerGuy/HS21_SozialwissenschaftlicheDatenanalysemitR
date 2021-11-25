2ffd053b61df6e10493b25e60033021d03b96deb
Schwegler Guy <SchweglG@unet.unilu.ch>
2021-11-25 12:17:28 +0100

    Update Bookdown WP10

--- Daten/ess_import.R
+++ Daten/ess_import.R
@@ -33,6 +33,54 @@ daten_ess$eduyrs[daten_ess$x==1482] <- round(mean(daten_ess$eduyrs[daten_ess$edu
                                      na.rm = T))
 
 # Faktoren definieren
-## folgt in der naechsten Einheit
+# Faktoren definieren
+## ungeordnete Faktoren
+daten_ess$gndr <- factor(daten_ess$gndr, 
+                         levels = c(1,2),
+                         labels = c("Male", "Female"))
+daten_ess$chldhm <- factor(daten_ess$chldhm, 
+                           levels = c(0,1),
+                           labels = c("Respondent lives with children at household grid",
+                                      "Does not"))
+daten_ess$pdwrk <- factor(daten_ess$pdwrk, 
+                          levels = c(1,2),
+                          labels = c("Not marked",
+                                     "Marked"))
+daten_ess$edctn <- factor(daten_ess$edctn, 
+                          levels = c(1,2),
+                          labels = c("Not marked",
+                                     "Marked"))
+daten_ess$uemp3m <- factor(daten_ess$uemp3m, 
+                           levels = c(1,2,7,8),
+                           labels = c("Yes", "No",
+                                      "Refusal", "Don't know"))
+## geordnete Faktoren
+daten_ess$polintr <- factor(daten_ess$polintr, 
+                            levels = c(4,3,2,1),
+                            labels = c("Not at all interested", 
+                                       "Hardly interested", 
+                                       "Quite interested", 
+                                       "Very interested"),
+                            ordered = T)
+daten_ess$edulvla <- factor(daten_ess$edulvla,
+                            levels = c(1,2,3,4,5),
+                            labels = c("ISCED 0-1",
+                                       "ISCED 2",
+                                       "ISCED 3",
+                                       "ISCED 4",
+                                       "ISCED 5-6"),
+                            ordered = T)
+
+daten_ess$eisced <- factor(daten_ess$eisced,
+                           levels = c(0,1,2,3,4,5,6,7),
+                           labels = c("Not possible to harmonise into ES-ISCED",
+                                      "ES-ISCED I",
+                                      "ES-ISCED II",
+                                      "ES-ISCED IIIb",
+                                      "ES-ISCED IIIa",
+                                      "ES-ISCED IV",
+                                      "ES-ISCED V1",
+                                      "ES-ISCED V2"),
+                           ordered = T)
 
 
